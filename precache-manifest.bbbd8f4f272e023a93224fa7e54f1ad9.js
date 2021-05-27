fa184c568f3bab1f3ccd5081d337f5d836adc049
ViPiven <vitalijjpiven777@gmail.com>
2021-05-27 22:09:34 +0300

    Updates

---
+++ precache-manifest.bbbd8f4f272e023a93224fa7e54f1ad9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcba196f08c05221f21ed3a0f1fea742",
+    "url": "./index.html"
+  },
+  {
+    "revision": "932095c9659ce14cf64f",
+    "url": "./static/css/main.3785dc88.chunk.css"
+  },
+  {
+    "revision": "5cfdcbeef26af9a9a716",
+    "url": "./static/js/2.f6b69482.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.f6b69482.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "932095c9659ce14cf64f",
+    "url": "./static/js/main.b6df9946.chunk.js"
+  },
+  {
+    "revision": "de889ebc4391832068f6",
+    "url": "./static/js/runtime-main.311b6371.js"
+  }
+]);
